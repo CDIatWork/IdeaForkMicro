43da9264777f9c8a97c57d0dfb83ee54fc715f59
Gerhard Petracek <gerhard.petracek@irian.at>
2017-07-07 12:38:03 +0200

    customized port

--- backend/ideafork_user-service/src/dev-starter/java/at/irian/cdiatwork/ideafork/dev/DevUserServerStarter.java
+++ backend/ideafork_user-service/src/dev-starter/java/at/irian/cdiatwork/ideafork/dev/DevUserServerStarter.java
@@ -1,9 +1,14 @@
 package at.irian.cdiatwork.ideafork.dev;
 
+import org.apache.deltaspike.core.api.config.ConfigResolver;
 import org.apache.meecrowave.Meecrowave;
 
 public class DevUserServerStarter {
     public static void main(String[] args) {
-        new Meecrowave().bake().await();
+        Meecrowave.Builder builder = new Meecrowave.Builder();
+        String configuredPort = ConfigResolver.getPropertyValue("user-service.http.port");
+        builder.setHttpPort(Integer.parseInt(configuredPort));
+
+        new Meecrowave(builder).bake().await();
     }
 }
